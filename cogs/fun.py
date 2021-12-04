52000f3af098716e5c610168833d8beda853b90a
TheNicoPanda <45668110+thenicopanda@users.noreply.github.com>
2021-12-04 10:47:53 -0800

    Removed Urban

--- cogs/fun.py
+++ cogs/fun.py
@@ -2,7 +2,6 @@
 ####################################################
  Commands
  * headsOrTails - Return Either Heads or Tails
- * urban - Search for a Term on Urban Dictionary
  * 8ball - Return an Eight Ball Response
 ####################################################
 """
@@ -31,43 +30,6 @@ class Fun(commands.Cog, name="Fun Stuff"):
             await ctx.respond("Heads!")
         else:
             await ctx.respond("Tails!")
-
-    @commands.slash_command(name='urban',
-                            guild_ids=[901328556603367446]
-                            )
-    async def urban(self, 
-                    ctx, 
-                    term: Option(str, "Term to search for.")
-                    ):
-        """Search for a Term on Urban Dictionary"""
-        term = " ".join(term)
-        msg = await ctx.respond("Searching...")
-        #  The headers required to recieve the information
-        headers={
-                "X-RapidAPI-Host": "mashape-community-urban-dictionary.p.rapidapi.com",
-                "X-RapidAPI-Key": "34176a38aamsh1a3cc0a94ed8bcep1c92b1jsn54239f977d2b"
-            }
-        #  Get the response
-        response = requests.get(f"https://mashape-community-urban-dictionary.p.rapidapi.com/define?term={term}", headers=headers)
-        #  Convert the response to a dictionary
-        response = json.loads(response.text)
-        #  Get the number of definitions
-        lenOfResponse = len(response['list'])
-        #  If there are results
-        if lenOfResponse > 0:
-            #  Randomly pick a response, subtract one from the number of
-            #  definitions so that it dosen't go past the final term
-            responseNum = random.randint(0, lenOfResponse - 1)
-            #  Save the definition
-            responseDef = response['list'][responseNum]['definition']
-            #  Save the author
-            responseAuthor = response['list'][responseNum]['author']
-            embed = discord.Embed(title = term, description = responseDef)
-            embed.set_footer(text=responseAuthor)
-            await msg.edit_original_message(embed=embed, content=None)
-        #  No results
-        else:
-            await msg.edit_original_message(content="No results found...")
 
 
     @commands.slash_command(name='8ball',
