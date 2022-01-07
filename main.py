40b68b11014a4f5a94ea1e0b1d66adf06b6eddb0
TheNicoPanda <45668110+thenicopanda@users.noreply.github.com>
2022-01-06 17:10:58 -0800

    Updates
    Added a check so that bot will not restart leaderboard if it's already running, also added a bit to the leaderboard update that will add needed roles to the user

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Imports
+from logging import exception
 import discord, random
 from discord.ext import commands, tasks
+from discord.utils import get
 import tools as t
 from decimal import Decimal
 
@@ -24,12 +26,14 @@ if __name__ == '__main__':
 async def on_ready():
     await bot.change_presence(status=discord.Status.online)
     print("Successfully logged in and booted... Now go fix some bugs!")
-    changeStatus.start()
+    if not changeStatus.is_running():
+        changeStatus.start()
 
 
 @tasks.loop(minutes=7)
 async def changeStatus():
     """Update the leaderboard and change the bot game every 7 minutes"""
+    guild = bot.get_guild(901328556603367446)
     await bot.change_presence(activity=discord.Game(random.choice(gameList)))
     leaderboardChannel = bot.get_channel(901710731848867900) # Get the #leaderboard channel
     sortedLeaderboard = t.updateLeaderboard() # Collect the current leaderboard stats
@@ -44,7 +48,13 @@ async def changeStatus():
             await leaderboardChannel.send(embed=initialLeaderboardEmbed) # Send the first embed
             embedLimitCounter = 0 # Reset the embed limit
             initialLeaderboardEmbed = discord.Embed(color=0xb3e4f4) # Create a new leaderboard embed
-
+        try: 
+            member = await guild.fetch_member(x["discord"])
+            
+            role = get(guild.roles, name=x['rank'])
+            await member.add_roles(role, reason="Leaderboard Update.")
+        except:
+            print(f"Could not give role '{x['rank']}' to {x['discord']}")
         eb = t.human_format(Decimal(x["eb"]))
         embedTitle = "{}. {}".format(countingNumber, x["nickname"]) # Set the title of the embed field
         initialLeaderboardEmbed.add_field(name=embedTitle, value=eb, inline=False) # add the field to the embed
